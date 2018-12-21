34a15395edfa4d088c1455bb8eb489b0682c6f99
Florent Teissier <teissierflorent@gmail.com>
2018-12-21 14:51:04 +0100

    indentation

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ done
 
 #link the config folder
 echo "creating the symlink to config folder"
-  read -p "> Are you sure? (y/n) " -n 1 -r
-  echo
-  if [[ $REPLY == "y" ]]
-  then
-    ln -s "$HOME/dotfiles/config" "$HOME/.config"
-  fi
+read -p "> Are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY == "y" ]]
+then
+  ln -s "$HOME/dotfiles/config" "$HOME/.config"
+fi
 
 #link the bin folder
 echo "creating the symlink to bin folder"
-  read -p "> Are you sure? (y/n) " -n 1 -r
-  echo
-  if [[ $REPLY == "y" ]]
-  then
-    ln -s "$HOME/dotfiles/bin" "$HOME/bin"
-  fi
+read -p "> Are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY == "y" ]]
+then
+  ln -s "$HOME/dotfiles/bin" "$HOME/bin"
+fi
