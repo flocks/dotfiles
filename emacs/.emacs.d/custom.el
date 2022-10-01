18e6221cc61767b8f4f93b19dc31dead0fb77c21
Florent Teissier <teissierflorent@gmail.com>
2022-10-01 13:14:00 +0200

    update custom

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("83fc7ca4d73cee934d2a96d5f2f6fe243afede055497528ab89dbb92c1145020" default))
+   '("ebcb78e41027f498a1ee53cd4ae41ebbf4c44841fe0ed787affd01ba5013c66b" "9b7d703afa15e320af9aa6b3e02bb1ebb8657c23043f7054357f86ce014b5461" "d983956f195a1a5e920c78216e6c164f4fd73a4fdce3f532d527182409f96515" "83fc7ca4d73cee934d2a96d5f2f6fe243afede055497528ab89dbb92c1145020" default))
  '(dashub--notify-delay 60)
  '(tab-width 4)
  '(warning-suppress-log-types '((comp))))
