4c3649e4209f2dc380e442301063a97ed941c9c8
Florent Teissier <teissierflorent@gmail.com>
2022-11-02 10:08:12 +0100

    add emacs function to edit file with sudo

--- emacs/.emacs.d/ft/ft-system.el
+++ emacs/.emacs.d/ft/ft-system.el
@@ -36,5 +36,21 @@
 	(evil-define-key 'motion map (kbd "RET") #'xinput-set-prop))
   )
 
+(set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
+(require 'tramp)
+(defun sudo-edit-current-file ()
+  (interactive)
+  (let ((position (point)))
+    (find-alternate-file
+     (if (file-remote-p (buffer-file-name))
+         (let ((vec (tramp-dissect-file-name (buffer-file-name))))
+           (tramp-make-tramp-file-name
+            "sudo"
+            (tramp-file-name-user vec)
+            (tramp-file-name-host vec)
+            (tramp-file-name-localname vec)))
+       (concat "/sudo:root@localhost:" (buffer-file-name))))
+    (goto-char position)))
+
 (provide 'ft-system)
 ;;; ft-system ends here
