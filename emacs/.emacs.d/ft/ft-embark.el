16780edde0185c758447d593cdba8a7b5da9dcdc
Florent Teissier <teissierflorent@gmail.com>
2022-12-21 23:28:39 +0100

    add a way to quickly go back to root/git/packag folder in minibuffer

--- emacs/.emacs.d/ft/ft-embark.el
+++ emacs/.emacs.d/ft/ft-embark.el
@@ -13,6 +13,7 @@
   :config
   (vertico-mode)
   (define-key vertico-map (kbd "M-r") 'consult-history)
+  (define-key minibuffer-mode-map (kbd "M-r") 'consult-history)
   (define-key minibuffer-local-shell-command-map (kbd "M-r") 'consult-history))
 
 
@@ -100,5 +101,29 @@ targets."
 
 (advice-add #'embark-completing-read-prompter
             :around #'embark-hide-which-key-indicator)
+
+
+
+(defun ft-add-trailing-slash (dir)
+  (if (string-suffix-p "/" dir)
+	  dir
+    (concat dir "/")))
+
+(defun ft-insert-previous-path ()
+  (interactive)
+  (when-let ((new-content (ft-minibuffer--get-new-path (minibuffer-contents))))
+	(delete-minibuffer-contents)
+	(insert-and-inherit new-content)))
+
+(defun ft-minibuffer--get-new-path (current-path)
+  (let ((project
+		 (locate-dominating-file current-path "package.json")))
+	(if (string= (ft-add-trailing-slash current-path) project)
+		(locate-dominating-file current-path ".git")
+	  (or project (locate-dominating-file current-path ".git") "~"))))
+
+(define-key vertico-map (kbd "M-u") 'ft-insert-previous-path)
+
+
 
 (provide 'ft-embark)
