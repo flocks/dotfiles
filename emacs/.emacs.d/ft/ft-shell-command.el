abeedf8e7bf8a7d8e12fea513859dbc87277565e
Florent Teissier <teissierflorent@gmail.com>
2023-05-10 22:38:15 +0200

    emacs: remove shelldon.el for now

--- emacs/.emacs.d/ft/ft-shell-command.el
+++ emacs/.emacs.d/ft/ft-shell-command.el
@@ -1,17 +1,5 @@
 (setq shell-command-prompt-show-cwd t)
 (setq enable-recursive-minibuffers t)
 
-(use-package shelldon
-  :straight (shelldon :type git
-                      :host github
-                      :repo "Overdr0ne/shelldon"
-                      :branch "master"
-                      :files ("shelldon.el"))
-  :config
-  (global-set-key (kbd "M-&") 'shelldon)
-  (define-key shelldon-mode-map (kbd "C-c C-c") 'compilation-minor-mode)
-  (define-key shelldon-minibuffer-local-command-map (kbd "C-;") 'shelldon-cd))
-
-
 (provide 'ft-shell-command)
 
