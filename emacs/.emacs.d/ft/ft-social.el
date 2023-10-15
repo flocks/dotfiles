200760762cfe64028a1a673b612695773972f539
Florent Teissier <teissierflorent@gmail.com>
2023-10-15 18:15:20 +0200

    some little elisp function to upload snippets/image to personal server

--- emacs/.emacs.d/ft/ft-social.el
+++ emacs/.emacs.d/ft/ft-social.el
@@ -58,5 +58,45 @@
   ;; (define-key 'normal bongo-mode-map (kbd "o") 'bongo-insert-file)
   (evil-define-key 'normal bongo-mode-map (kbd "o") 'bongo-insert-file))
 
+(defun ft-upload-content-mirc (content)
+  "Send a POST request with content as data in plain text."
+  (let ((url-request-method "POST")
+		(url-request-extra-headers `(("Content-Type" . "text/plain")))
+		(url-request-data content))
+	(url-retrieve "http://flocks.dev:8080"
+				  (lambda (status)
+					(goto-char (point-min))
+					(re-search-forward "^$")
+					(delete-region (point) (point-min))
+					(let* ((response (json-parse-buffer :object-type 'alist))
+						   (id (alist-get 'id response))
+						   (url (format "http://flocks.dev:8080/view/%s" id)))
+					  (message url)
+					  (kill-new url))
+					)
+				  )))
+
+(defun ft-upload-snippet (beginning end)
+  "Take the region and upload it to server"
+  (interactive "r")
+  (if (use-region-p)
+	  (let ((text (buffer-substring-no-properties beginning end)))
+		(ft-upload-content-mirc text))))
+
+(defun ft-upload-image ()
+  "Take the image under cursor and send its base64 representation to
+server"
+  (interactive)
+  (unless (eq major-mode 'dired-mode)
+	(error "Not in a dired buffer"))
+  (let ((files (dired-get-marked-files)))
+	(unless (= (length files) 1)
+	  (error "Multiple files marked, only support one file"))
+	(ft-upload-content-mirc (shell-command-to-string (format "base64 %s" (car files))))))
+
+(use-package hackernews
+  :straight t)
+
+
 
 (provide 'ft-social)
