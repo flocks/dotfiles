47a328317e2b83700f7505be197dc79c0708f97f
Florent Teissier <teissierflorent@gmail.com>
2024-02-20 19:20:50 +0100

    [emacs] remove embark-indicator related code

--- emacs/.emacs.d/ft/ft-embark.el
+++ emacs/.emacs.d/ft/ft-embark.el
@@ -27,14 +27,6 @@
   :bind
   (("C-=" . embark-act)      
    ("C-c C-o" . embark-export))       
-
-  :init
-  :config
-  ;; Hide the mode line of the Embark live/completions buffers
-  ;; (add-to-list 'display-buffer-alist
-  ;;              '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-  ;;                nil
-  ;;                (window-parameters (mode-line-format . none))))
 )
 
 ;; Consult users will also want the embark-consult package.
@@ -69,51 +61,12 @@
 
 (global-set-key (kbd "C-s") 'consult-line)
 
-(defun embark-which-key-indicator ()
-  "An embark indicator that displays keymaps using which-key.
-The which-key help message will show the type and value of the
-current target followed by an ellipsis if there are further
-targets."
-  (lambda (&optional keymap targets prefix)
-    (if (null keymap)
-        (which-key--hide-popup-ignore-command)
-      (which-key--show-keymap
-       (if (eq (plist-get (car targets) :type) 'embark-become)
-           "Become"
-         (format "Act on %s '%s'%s"
-                 (plist-get (car targets) :type)
-                 (embark--truncate-target (plist-get (car targets) :target))
-                 (if (cdr targets) "…" "")))
-       (if prefix
-           (pcase (lookup-key keymap prefix 'accept-default)
-             ((and (pred keymapp) km) km)
-             (_ (key-binding prefix 'accept-default)))
-         keymap)
-       nil nil t (lambda (binding)
-                   (not (string-suffix-p "-argument" (cdr binding))))))))
-
-(setq embark-indicators
-  '(embark-which-key-indicator
-    embark-highlight-indicator
-    embark-isearch-highlight-indicator))
-
-(defun embark-hide-which-key-indicator (fn &rest args)
-  "Hide the which-key indicator immediately when using the completing-read prompter."
-  (which-key--hide-popup-ignore-command)
-  (let ((embark-indicators
-         (remq #'embark-which-key-indicator embark-indicators)))
-      (apply fn args)))
-
-(advice-add #'embark-completing-read-prompter
-            :around #'embark-hide-which-key-indicator)
-
 
 (use-package monobuffer
   :straight (monobuffer :type git :host github :repo "flocks/monobuffer.el")
   :config
   (define-key vertico-map (kbd "M-u") 'monobuffer)
-  (define-key vertico-map (kbd "M-U") 'monobuffer-root)
-  )
+  (define-key vertico-map (kbd "M-U") 'monobuffer-root))
 
 (use-package marginalia
   :straight t
