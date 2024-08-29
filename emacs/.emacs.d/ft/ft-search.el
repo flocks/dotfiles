26b14715c36c673ad57aad771f29683f7a5a2f15
Florent Teissier <teissierflorent@gmail.com>
2024-08-29 21:44:48 +0200

    emacs: remove unused code in ft-search

--- emacs/.emacs.d/ft/ft-search.el
+++ emacs/.emacs.d/ft/ft-search.el
@@ -1,38 +1,3 @@
-;; find a way to perform rg on list of file, list of marked files
-
-(use-package rg
-  :straight t
-  :config
-  (rg-enable-menu)
-  (rg-define-toggle "--context 3" (kbd "C-c c"))
-  (setq rg-group-result nil) ;; much faster with large result
-  )
-
-(defun ft-run-occur (arg)
-  "Run occur, with pre-selected term if no prefix arg"
-  (interactive "P")
-  (if arg
-	  (call-interactively #'occur)
-	(occur (thing-at-point 'word))))
-
-(define-key prog-mode-map (kbd "C-c o") 'ft-run-occur)
-
-(eval-after-load 'ft-dired
-  (progn 
-	(defun ft-my-consult-ripgrep ()
-	  (interactive)
-	  (if (and (eq major-mode 'dired-mode)
-			   (ft-dired-is-mark-active))
-		  (let* ((files (string-join (dired-get-marked-files) " "))
-				 (consult-ripgrep-args
-				  (format "%s %s"
-						  (string-join (butlast (split-string consult-ripgrep-args)) " ")
-						  files)))
-			(call-interactively #'consult-ripgrep)) 
-		(call-interactively #'consult-ripgrep)))
-
-	(evil-define-key '(motion normal) global-map (kbd "C-f") 'ft-my-consult-ripgrep)))
-
 (defun ft--sanitize-grep-filename (filename)
   "Just clear filename from `:line:col' that is in the grep buffer"
   (car (split-string filename ":")))
@@ -51,10 +16,6 @@ dired buffer"
 		(forward-line 1))
 	  (dired (cons "Grep" (cl-remove-duplicates files :test 'string=))))))
 
-(defun rgl (dir regexp)
-  (interactive "DFind-grep (directory): \nsFind-grep (grep regexp): ")
-  (let ((default-directory dir))
-	(dired (cons (format "*rgl* - %s" regexp) (process-lines "rg" "-l" regexp)))))
 
 (define-key grep-mode-map (kbd "C-c d") 'ft-grep-to-dired)
 (evil-define-key 'normal grep-mode-map (kbd "g r") 'embark-rerun-collect-or-export)
