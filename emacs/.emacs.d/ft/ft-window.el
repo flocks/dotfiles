f86263b536bd632e36f00eb7914c75db2e9b6958
Florent Teissier <teissierflorent@gmail.com>
2024-09-10 16:42:25 +0200

    emacs: clean ft-window

--- emacs/.emacs.d/ft/ft-window.el
+++ emacs/.emacs.d/ft/ft-window.el
@@ -16,32 +16,25 @@
 (global-set-key (kbd "M-4") 'other-window-prefix)
 
 
-
-(use-package winner
-   :straight t
-   :config
-    (winner-mode)
-    (global-set-key (kbd "s-p") 'winner-undo)
-    (global-set-key (kbd "s-n") 'winner-redo))
-
 (use-package hydra
   :straight t)
 
 (use-package buffer-move
-  :straight t)
+  :straight t
+  :config
+  (defhydra hydra-windows (global-map "C-c w")
+	"window"
 
-(defhydra hydra-windows (global-map "C-c w")
-  "window"
+	("J" evil-window-increase-height)
+	("K" evil-window-decrease-height)
+	("L" evil-window-increase-width)
+	("H" evil-window-decrease-width)
 
-  ("J" evil-window-increase-height)
-  ("K" evil-window-decrease-height)
-  ("L" evil-window-increase-width)
-  ("H" evil-window-decrease-width)
+	("j" buf-move-down)
+	("k" buf-move-up)
+	("l" buf-move-right)
+	("h" buf-move-left)))
 
-  ("j" buf-move-down)
-  ("k" buf-move-up)
-  ("l" buf-move-right)
-  ("h" buf-move-left))
 
 (use-package winner
    :straight t
@@ -50,39 +43,6 @@
     (global-set-key (kbd "s-p") 'winner-undo)
     (global-set-key (kbd "s-n") 'winner-redo))
 
-
-(defun ft-i3-move-down ()
-  (condition-case nil
-      (windmove-down)
-    ((debug error)
-     (shell-command "i3-msg focus down"))))
-
-(defun ft-i3-move-up ()
-  (condition-case nil
-      (windmove-up)
-    ((debug error)
-     (shell-command "i3-msg focus up"))))
-
-(defun ft-i3-move-right ()
-  (condition-case nil
-      (windmove-right)
-    ((debug error)
-     (shell-command "i3-msg focus right"))))
-
-(defun ft-i3-move-left ()
-  (condition-case nil
-      (windmove-left)
-    ((debug error)
-     (shell-command "i3-msg focus left"))))
-
-
-(global-set-key (kbd "s-J") 'evil-window-move-very-bottom)
-(global-set-key (kbd "s-K") 'evil-window-move-very-top)
-
-(global-set-key (kbd "s-C-h") 'enlarge-window-horizontally)
-(global-set-key (kbd "s-C-j") 'enlarge-window)
-(global-set-key (kbd "s-C-k") 'shrink-window)
-(global-set-key (kbd "s-C-l") 'shrink-window-horizontally)
 (global-set-key (kbd "C-x RET") 'tear-off-window)
 
 (provide 'ft-window)
