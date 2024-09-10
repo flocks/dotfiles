bac5952ce7beb1d5e640d04b381022e1f04dece1
Florent Teissier <teissierflorent@gmail.com>
2024-09-10 16:45:16 +0200

    fix indent

--- emacs/.emacs.d/ft/ft-jira.el
+++ emacs/.emacs.d/ft/ft-jira.el
@@ -1,12 +1,9 @@
 (defun ft-jira--get-basic-auth-token ()
   (let ((token (funcall
-				(plist-get (nth 0
-								(auth-source-search :host "ledgerhq.atlassian.net"
-													:user "florent.teissier@ledger.fr"))
-						   :secret))))
-	(format "Basic %s"
-			(shell-command-to-string
-			 (format "echo -n florent.teissier@ledger.fr:%s | base64 -w0" token)))))
+                (plist-get (nth 0 (auth-source-search :host "ledgerhq.atlassian.net" :user "florent.teissier@ledger.fr")) :secret))))
+    (format "Basic %s"
+            (shell-command-to-string
+             (format "echo -n florent.teissier@ledger.fr:%s | base64 -w0" token)))))
 
 
 (defun ft-jira--extract-json ()
@@ -14,6 +11,7 @@
   (re-search-forward "^$")
   (delete-region (point) (point-min))
   (json-parse-buffer :object-type 'alist))
+
 (defun ft-jira-issue-summary (issue)
   "Fetch summary of issue"
   (interactive "sIssue: ")
@@ -31,7 +29,7 @@
   (unless (use-region-p)
 	(user-error "Nothing selected"))
   (let* ((issue (buffer-substring-no-properties beg end))
-		(browse-url (format "https://ledgerhq.atlassian.net/browse/%s" issue)))
+		 (browse-url (format "https://ledgerhq.atlassian.net/browse/%s" issue)))
 	(ft-jira-issue-summary issue)
 	(kill-new browse-url)))
 
