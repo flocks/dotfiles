a2cdb128987935c081f7ea729040fb9f744295b2
Florent Teissier <teissierflorent@gmail.com>
2024-10-07 17:27:36 +0200

    emacs: bind ft-grep-to-dired to compilation mode
    As I'm more and more using plain old compilation mode to interact with
    grep/rg it makes sense to have the binding there

--- emacs/.emacs.d/ft/ft-search.el
+++ emacs/.emacs.d/ft/ft-search.el
@@ -36,7 +36,7 @@ dired buffer"
       (dired (cons "Grep" (cl-remove-duplicates files :test 'string=))))))
 
 
-(define-key grep-mode-map (kbd "C-c d") 'ft-grep-to-dired)
+(define-key compilation-mode-map (kbd "C-c d") 'ft-grep-to-dired)
 (evil-define-key 'normal grep-mode-map (kbd "g r") 'embark-rerun-collect-or-export)
 
 
