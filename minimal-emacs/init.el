48e31a8a012ea64135340b593b79b828c2a6eade
Florent Teissier <teissierflorent@gmail.com>
2025-09-06 21:12:19 +0200

    [minimal-emacs] disable evil for now

--- minimal-emacs/init.el
+++ minimal-emacs/init.el
@@ -143,36 +143,40 @@
 
 
 
-(use-package evil
-  :straight t ;; install the evil package if not installed
-  :init ;; tweak evil's configuration before loading it
+;; evil setup
+;; (use-package evil
+;;   :straight t ;; install the evil package if not installed
+;;   :init ;; tweak evil's configuration before loading it
 
-  (setq evil-ex-complete-emacs-commands nil)
-  (setq evil-vsplit-window-right t)
-  (setq evil-split-window-below t)
-  (setq evil-want-keybinding nil)
+;;   (setq evil-ex-complete-emacs-commands nil)
+;;   (setq evil-vsplit-window-right t)
+;;   (setq evil-split-window-below t)
+;;   (setq evil-want-keybinding nil)
 
-  :config ;; tweak evil after loading it
-  (add-hook 'view-mode-hook 'evil-local-mode)
-  (add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?- "w")))
-  (add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
-  (evil-mode))
+;;   :config ;; tweak evil after loading it
+;;   (add-hook 'view-mode-hook 'evil-local-mode)
+;;   (add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?- "w")))
+;;   (add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
+;;   (evil-mode))
 
-(use-package evil-commentary
-  :after evil
-  :straight t
-  :init (evil-commentary-mode))
+;; (use-package evil-commentary
+;;   :after evil
+;;   :straight t
+;;   :init (evil-commentary-mode))
 
-(use-package evil-surround
-  :straight t
-  :config
-  (global-evil-surround-mode 1))
+;; (use-package evil-surround
+;;   :straight t
+;;   :config
+;;   (global-evil-surround-mode 1))
 
-(use-package evil-collection
-  :after evil
-  :straight t
-  :config
-  (evil-collection-init))
+;; (use-package evil-collection
+;;   :after evil
+;;   :straight t
+;;   :config
+;;   (evil-collection-init))
+
+
+;; end evil setup
 
 ;; buffer
 (global-set-key (kbd "C-x k") 'kill-buffer-and-window)
