2df093d5ffcd0f4857baa8efeabeb751c414c7e0
Florent Teissier <teissierflorent@gmail.com>
2026-02-19 23:04:50 +0100

    [emacs] multi keys for ft-jump for ledger projects

--- emacs/.emacs.d/ft/ft-jump.el
+++ emacs/.emacs.d/ft/ft-jump.el
@@ -1,18 +1,29 @@
 (use-package hydra
   :straight t)
 
+(defhydra hydra-jump-m (evil-motion-state-map "C-e")
+  "Jump multisig"
+  ("m" (lambda () (interactive) (find-file "~/ledger/les-multisig")) :exit t)
+  ("w" (lambda () (interactive) (find-file "~/ledger/les-multisig/packages/web")) :exit t)
+  ("a" (lambda () (interactive) (find-file "~/ledger/les-multisig/packages/api")) :exit t)
+)
+(defhydra hydra-jump-r (evil-motion-state-map "C-e")
+  "Jump revault"
+  ("r" (lambda () (interactive) (find-file "~/ledger/revault")) :exit t)
+  ("a" (lambda () (interactive) (find-file "~/ledger/revault/packages/api")) :exit t)
+  ("w" (lambda () (interactive) (find-file "~/ledger/revault/packages/web")) :exit t)
+  ("m" (lambda () (interactive) (find-file "~/ledger/revault/packages/mobile")) :exit t)
+  ("u" (lambda () (interactive) (find-file "~/ledger/revault/packages/ui")) :exit t)
+  ("s" (lambda () (interactive) (find-file "~/ledger/revault/packages/sdk")) :exit t)
+  ("d" (lambda () (interactive) (find-file "~/ledger/revault/packages/doc")) :exit t)
+  )
+
 (defhydra hydra-jump (evil-motion-state-map "C-e")
   "Jump"
   ("0" (lambda () (interactive) (find-file "~/0xparser")) :exit t)
-  ("A" (lambda () (interactive) (find-file "~/ledger/revault/packages/api")) :exit t)
   ("D" (lambda () (interactive) (find-file "~/Downloads")) :exit t)
   ("L" (lambda () (interactive) (find-file "~/ledger/ledger-live")) :exit t)
-  ("M" (lambda () (interactive) (find-file "~/ledger/revault/packages/mobile")) :exit t)
-  ("R" (lambda () (interactive) (find-file "~/ledger/revault")) :exit t)
   ("S" (lambda () (interactive) (find-file "~/sandbox/main.ts")) :exit t)
-  ("U" (lambda () (interactive) (find-file "~/ledger/revault/packages/ui")) :exit t)
-  ("W" (lambda () (interactive) (find-file "~/ledger/revault/packages/web")) :exit t)
-  ("a" (lambda () (interactive) (find-file "~/ledger/vault-apdu-connector")) :exit t)
   ("c" (lambda () (interactive) (find-file "~/chessblind")) :exit t)
   ("d" (lambda () (interactive) (find-file "~/dotfiles")) :exit t)
   ("e" (lambda () (interactive) (find-file "~/ledger/vault-e2e-tests")) :exit t)
@@ -20,11 +31,13 @@
   ("g" (lambda () (interactive) (find-file "~/ledger/ledger-vault-api")) :exit t)
   ("h" (lambda () (interactive) (find-file "~")) :exit t)
   ("l" (lambda () (interactive) (find-file "~/ledger")) :exit t)
-  ("m" (lambda () (interactive) (find-file "~/ledger/les-multisig/packages/web")) :exit t)
+  ("m" hydra-jump-m/body "multisig" :exit t)
+  ("r" hydra-jump-r/body "rev" :exit t)
   ("p" (lambda () (interactive) (find-file "~/riverodds")) :exit t)
-  ("r" (lambda () (interactive) (find-file "~/ledger/vault-remote")) :exit t)
+  ("R" (lambda () (interactive) (find-file "~/ledger/vault-remote")) :exit t)
   ("s" (lambda () (interactive) (find-file "~/screenshots")) :exit t)
   ("v" (lambda () (interactive) (find-file "~/ledger/vault-ts")) :exit t)
+
   )
 
 (defun ft-diff-file-dwim ()
