acc4af0d2bbae5dc419f016fa24e84d24f1d7592
Florent Teissier <teissierflorent@gmail.com>
2026-03-31 18:00:52 +0200

    [emacs] binding for dabbrev in minibuffer

--- emacs/.emacs.d/ft/ft-misc.el
+++ emacs/.emacs.d/ft/ft-misc.el
@@ -60,6 +60,13 @@
 
 (add-hook 'minibuffer-setup-hook 'yas-minor-mode)
 
+(defun my-dabbrev-minibuffer-setup ()
+  "Enable dabbrev in minibuffer."
+  (local-set-key (kbd "M-/") 'dabbrev-expand)
+  (local-set-key (kbd "C-M-/") 'dabbrev-completion))
+
+(add-hook 'minibuffer-setup-hook 'my-dabbrev-minibuffer-setup)
+
 (defun ft-lorem ()
   (interactive)
   (insert "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"))
