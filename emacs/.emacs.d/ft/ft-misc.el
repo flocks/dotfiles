ef6a4473cdc8739203ac1b1c327de6df2d590d22
Florent Teissier <teissierflorent@gmail.com>
2026-06-07 19:07:29 +0200

    [emacs] indicator in modeline when TRAMP is on

--- emacs/.emacs.d/ft/ft-misc.el
+++ emacs/.emacs.d/ft/ft-misc.el
@@ -84,5 +84,15 @@
   (interactive)
   (insert "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"))
 
+
+(use-package evil-mc
+  :straight t
+  )
+
+(setq-default mode-line-remote
+			  '(:eval
+				(when (tramp-tramp-file-p (or (buffer-file-name) (dired-current-directory)))
+				  (propertize " TRAMP " 'face '(:background "red" :foreground "white")))))
+
 (provide 'ft-misc)
 
