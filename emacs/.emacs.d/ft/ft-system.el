ad308a5f68f7049de2cf9eb1fe9cae20990eead9
Florent Teissier <teissierflorent@gmail.com>
2026-07-22 16:23:54 +0200

    [emacs] nginx-mode + add ft-sudo-this-file

--- emacs/.emacs.d/ft/ft-system.el
+++ emacs/.emacs.d/ft/ft-system.el
@@ -6,5 +6,13 @@
   (setq proced-enable-color-flag t)
   (setq proced-goal-attribute nil))
 
+(use-package nginx-mode
+  :straight t)
+
+(defun ft-sudo-this-file ()
+  (interactive)
+  (let ((file (buffer-file-name (current-buffer))))
+	(find-file (format "/sudo::%s" file))))
+
 (provide 'ft-system)
 ;;; ft-system ends here
